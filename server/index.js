f334723baf83601c63724452d0cea0c03406c7b8
ArpitGupta0212 <arpitboss52525@gmail.com>
2025-07-08 12:25:56 +0530

    backend_almost_done

--- server/index.js
+++ server/index.js
@@ -1,35 +1,25 @@
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
-const itemRoutes = require('./routes/items');
-const paymentRoutes = require('./routes/payment');
-const setupSockets = require('./sockets/bids');
+const routes = require('./routes');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const PORT = process.env.PORT || 3000;
 
+mongoose.connect('Your MongoDB URI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use('/', routes);
 
-mongoose.connect('mongodb://localhost:27017/auctionDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
-app.use('/items', itemRoutes);
-app.use('/pay', paymentRoutes);
-
-
-setupSockets(io);
-
-
-const PORT = 5000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
